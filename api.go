a094e5143f521e35b6a2b2812668ac3fe95bf7bf
actiotech <faisal.kk@actiotech.com>
2015-08-24 16:30:42 +0530

    Adding api functions

--- api.go
+++ api.go
@@ -9,5 +9,10 @@ func GetDateFromTime(time.Time) Date
 //Random dates.Range of date is from 0 to now.(see time packege documentation for 0 time in Go)
 func GetRandomDates(Count int)
 
+//Sunday=0
+func GetRandomWeekDays(WeekDay int)
+
+func GetRandomWeekDaysSorted(WeekDay int)
+
 //Random dates from from to To dates
 func GetRandomDatesInRange(Count int, From, To Date)
